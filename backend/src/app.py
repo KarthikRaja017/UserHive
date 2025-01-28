996f40cee482a8dca11dd7eb32e32316f4863cd5
KarthikRaja017 <karthikrajayuvaraj16@gmail.com>
2025-01-28 19:52:54 +0530

    Updated project description and other files

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from config.db import mongo
 
@@ -7,8 +8,18 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app, resources={r"/*": {"origins": "*"}})
     mongo.init_app(app)
+    frontend_folder = os.path.abspath(os.path.join(os.getcwd(), "..", "frontend"))
+    dist_folder = os.path.join(frontend_folder, "dist")
+    
+    @app.route("/", defaults={"filename": ""})
+    @app.route("/<path:filename>")
+    def index(filename):
+        if filename and os.path.exists(os.path.join(dist_folder, filename)):
+            return send_from_directory(dist_folder, filename)
+        else:
+            return send_from_directory(dist_folder, "index.html")
+        
     from users import users_bp
-
     app.register_blueprint(users_bp)
     return app
 
