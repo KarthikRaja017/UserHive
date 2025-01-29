d2b8b93471ec924733f985d968179a201c6d32ec
Karthik Raja Y <148568954+KarthikRaja017@users.noreply.github.com>
2025-01-29 21:19:49 +0530

    Update app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,16 +8,16 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     CORS(app, resources={r"/*": {"origins": "*"}})
     mongo.init_app(app)
-    # frontend_folder = os.path.abspath(os.path.join(os.getcwd(), "..", "frontend"))
-    # dist_folder = os.path.join(frontend_folder, "dist")
+    frontend_folder = os.path.abspath(os.path.join(os.getcwd(), "..", "frontend"))
+    dist_folder = os.path.join(frontend_folder, "dist")
     
-    # @app.route("/", defaults={"filename": ""})
-    # @app.route("/<path:filename>")
-    # def index(filename):
-    #     if filename and os.path.exists(os.path.join(dist_folder, filename)):
-    #         return send_from_directory(dist_folder, filename)
-    #     else:
-    #         return send_from_directory(dist_folder, "index.html")
+    @app.route("/", defaults={"filename": ""})
+    @app.route("/<path:filename>")
+    def index(filename):
+        if filename and os.path.exists(os.path.join(dist_folder, filename)):
+            return send_from_directory(dist_folder, filename)
+        else:
+            return send_from_directory(dist_folder, "index.html")
         
     from src.users import users_bp
     app.register_blueprint(users_bp)
