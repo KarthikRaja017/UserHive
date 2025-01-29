5ee1fb6bdfeb6a63ec8b136b07c5db4b101e3728
Karthik Raja Y <148568954+KarthikRaja017@users.noreply.github.com>
2025-01-29 18:58:09 +0530

    Update db.py

--- backend/src/config/db.py
+++ backend/src/config/db.py
@@ -13,24 +13,19 @@ class MongoDB:
             self.init_app(app)
 
     def init_app(self, app):
-        """Initialize MongoDB with the given Flask app."""
         self.app = app
 
     def initProductionDb(self, uri, db):
-        """Initialize a production database with a separate URI."""
         production_client = MongoClient(uri, tlsCAFile=certifi.where()) 
         return production_client[db]
 
     def getDb(self, dbName):
-        """Retrieve a specific database by name."""
         return self.client[dbName]
 
 
-# Initialize the MongoDB connection
+
 mongo = MongoDB()
 
-# Test MongoDB Connection
-db = mongo.db  # Default database
-print(db.list_collection_names())  # Prints available collections
+
 
 
